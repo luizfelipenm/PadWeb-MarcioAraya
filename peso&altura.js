2644acd239e2de559b1ea2de82d7eba5bab59c0a
luizfelipenm <111882138+luizfelipenm@users.noreply.github.com>
2022-11-01 23:09:46 -0300

    Add files via upload

---
+++ peso&altura.js
@@ -0,0 +1,12 @@
+let imc= peso / altura * altura
+let situacao=""; let cor=""; 
+    if (imc<17) {
+        situacao="Muito abaixo do peso";
+        cor="red";
+    } else if (imc>17 && imc>18.50) {
+        situacao="";
+        cor="orange";
+    } else if (imc>=18.50 && imc<25) {
+        situacao="Normal"
+        cor="green"
+    } else if ()
---
+++ pesoalturaformdificult.js
@@ -0,0 +1,14 @@
+let imc= peso / altura * altura
+let situacao=""; let cor=""; 
+    if (imc < 17) {
+        situacao="Muito abaixo do peso";
+        cor="red";
+    }
+    {else}
+        if (imc >=17 && imc<18.50) {
+            situacao="Abaixo do peso";
+            cor="orange";
+        }
+    {else}
+    
+
